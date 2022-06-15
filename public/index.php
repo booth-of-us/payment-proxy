b73ccba6d941fb6a3cb2d895b300147a134c7d10
aldiwildan77 <aldiwild77@gmail.com>
2022-06-15 09:25:53 +0700

    [feat] add 4 core function payment

--- public/index.php
+++ public/index.php
@@ -18,16 +18,121 @@ $app->add(new BasePathMiddleware($app));
 
 $app->addErrorMiddleware(true, true, true);
 
+$apiKey = 'api_key_anda';
+$host = 'https://tripay.co.id/api-sandbox';
+
 // Define app routes
 $app->get('/', function (Request $request, Response $response) {
   $response->getBody()->write('Hello, World!');
   return $response;
 })->setName('root');
 
-// TODO: Create Transaction
-// TODO: Get Payment Channels
-// TODO: Get Fee Calculator
-// TODO: Get Payment Instruction
+$app->get('/merchant/fee-calculator', function (Request $request, Response $response) {
+
+  $payload = [
+    'code' => $request->getQueryParams()['code'],
+    'amount' => $request->getQueryParams()['amount'],
+  ];
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, [
+    CURLOPT_FRESH_CONNECT  => true,
+    CURLOPT_URL            => $this->host . '/merchant/fee-calculator?' . http_build_query($payload),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER         => false,
+    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
+    CURLOPT_FAILONERROR    => false,
+    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+  ]);
+
+  $response = curl_exec($curl);
+  $error = curl_error($curl);
+
+  curl_close($curl);
+
+  if (!empty($error)) return $error;
+
+  return $response;
+})->setName('merchant-fee-calculator');
+
+$app->post('/transaction/create', function (Request $request, Response $response) {
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, [
+    CURLOPT_FRESH_CONNECT  => true,
+    CURLOPT_URL            => $this->host . '/transaction/create',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER         => false,
+    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
+    CURLOPT_FAILONERROR    => false,
+    CURLOPT_POST           => true,
+    CURLOPT_POSTFIELDS     => http_build_query($request->getBody()),
+    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+  ]);
+
+  $response = curl_exec($curl);
+  $error = curl_error($curl);
+
+  curl_close($curl);
+
+  if (!empty($error)) return $error;
+
+  return $response;
+})->setName('transaction-create');
+
+$app->get('/merchant/payment-channel', function (Request $request, Response $response) {
+
+  $payload = ['code' => $request->getQueryParams()['code']];
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_FRESH_CONNECT  => true,
+    CURLOPT_URL            => $this->host . '/merchant/payment-channel?' . http_build_query($payload),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER         => false,
+    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
+    CURLOPT_FAILONERROR    => false,
+    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+  ));
+
+  $response = curl_exec($curl);
+  $error = curl_error($curl);
+
+  curl_close($curl);
+
+  if (!empty($error)) return $error;
+
+  return $response;
+})->setName('merchant-payment-channel');
+
+$app->get('/payment/instruction', function (Request $request, Response $response) {
+
+  $payload = ['code' => $request->getQueryParams()['code']];
+
+  $curl = curl_init();
+
+  curl_setopt_array($curl, [
+    CURLOPT_FRESH_CONNECT  => true,
+    CURLOPT_URL            => $this->host . '/payment/instruction?' . http_build_query($payload),
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HEADER         => false,
+    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
+    CURLOPT_FAILONERROR    => false,
+    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+  ]);
+
+  $response = curl_exec($curl);
+  $error = curl_error($curl);
+
+  curl_close($curl);
+
+  if (!empty($error)) return $error;
+
+  return $response;
+})->setName('payment-instruction');
 
 // Run app
 $app->run();
