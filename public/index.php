8a22aa2cf2161fd75e77fd57a98317a3b8d82ee8
aldiwildan77 <aldiwild77@gmail.com>
2022-06-15 08:29:56 +0700

    [Chore] Initial Setup

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Add Slim routing middleware
+$app->addRoutingMiddleware();
+
+// Set the base path to run the app in a subdirectory.
+// This path is used in urlFor().
+$app->add(new BasePathMiddleware($app));
+
+$app->addErrorMiddleware(true, true, true);
+
+// Define app routes
+$app->get('/', function (Request $request, Response $response) {
+  $response->getBody()->write('Hello, World!');
+  return $response;
+})->setName('root');
+
+// TODO: Create Transaction
+// TODO: Get Payment Channels
+// TODO: Get Fee Calculator
+// TODO: Get Payment Instruction
+
+// Run app
+$app->run();
