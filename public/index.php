9eec2b15f81c92421fe2ad06ca76d04ad47f6709
aldiwildan77 <aldiwild77@gmail.com>
2022-06-16 23:01:52 +0700

    [feat] tripay payment done

--- public/index.php
+++ public/index.php
@@ -7,6 +7,10 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$configuration = [
+  'tripay_host' => 'https://tripay.co.id/api-sandbox'
+];
+
 $app = AppFactory::create();
 
 // Add Slim routing middleware
@@ -18,120 +22,202 @@ $app->add(new BasePathMiddleware($app));
 
 $app->addErrorMiddleware(true, true, true);
 
-$apiKey = 'api_key_anda';
-$host = 'https://tripay.co.id/api-sandbox';
-
 // Define app routes
 $app->get('/', function (Request $request, Response $response) {
   $response->getBody()->write('Hello, World!');
   return $response;
 })->setName('root');
 
-$app->get('/merchant/fee-calculator', function (Request $request, Response $response) {
+$app->get('/merchant/fee-calculator', function (Request $request, Response $response) use ($configuration) {
 
-  $payload = [
-    'code' => $request->getQueryParams()['code'],
-    'amount' => $request->getQueryParams()['amount'],
-  ];
+  $payload = [];
 
-  $curl = curl_init();
+  try {
+    if (array_key_exists('code', $request->getQueryParams())) {
+      $payload['code'] = $request->getQueryParams()['code'];
+    }
 
-  curl_setopt_array($curl, [
-    CURLOPT_FRESH_CONNECT  => true,
-    CURLOPT_URL            => $this->host . '/merchant/fee-calculator?' . http_build_query($payload),
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HEADER         => false,
-    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
-    CURLOPT_FAILONERROR    => false,
-    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
-  ]);
+    if (array_key_exists('amount', $request->getQueryParams())) {
+      $payload['amount'] = $request->getQueryParams()['amount'];
+    }
 
-  $response = curl_exec($curl);
-  $error = curl_error($curl);
+    $auth = isset($request->getHeader('Authorization')[0]) ? $request->getHeader('Authorization')[0] : '';
 
-  curl_close($curl);
+    $curl = curl_init();
 
-  if (!empty($error)) return $error;
+    curl_setopt_array($curl, [
+      CURLOPT_FRESH_CONNECT  => true,
+      CURLOPT_URL            => $configuration['tripay_host'] . '/merchant/fee-calculator?' . http_build_query($payload),
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HEADER         => false,
+      CURLOPT_HTTPHEADER     => ['Authorization: ' . $auth],
+      CURLOPT_FAILONERROR    => false,
+      CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+    ]);
 
-  return $response;
+    $curlResult = curl_exec($curl);
+    $error = curl_error($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+    curl_close($curl);
+
+    if (!empty($error)) {
+      $response->withStatus(500)->getBody()->write($error);
+      return $response->withHeader('Content-Type', 'application/json');
+    }
+
+    $resultData = json_decode($curlResult, true);
+    if ($resultData['success'] == false) {
+      $response->getBody()->write($curlResult);
+      return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
+    }
+
+    $response->getBody()->write($curlResult);
+    return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+  } catch (Exception $e) {
+    $response->getBody()->write($e->getMessage());
+    return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+  }
 })->setName('merchant-fee-calculator');
 
-$app->post('/transaction/create', function (Request $request, Response $response) {
+$app->post('/transaction/create', function (Request $request, Response $response) use ($configuration) {
 
-  $curl = curl_init();
+  try {
+    $auth = isset($request->getHeader('Authorization')[0]) ? $request->getHeader('Authorization')[0] : '';
 
-  curl_setopt_array($curl, [
-    CURLOPT_FRESH_CONNECT  => true,
-    CURLOPT_URL            => $this->host . '/transaction/create',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HEADER         => false,
-    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
-    CURLOPT_FAILONERROR    => false,
-    CURLOPT_POST           => true,
-    CURLOPT_POSTFIELDS     => http_build_query($request->getBody()),
-    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
-  ]);
+    $curl = curl_init();
 
-  $response = curl_exec($curl);
-  $error = curl_error($curl);
+    curl_setopt_array($curl, [
+      CURLOPT_FRESH_CONNECT  => true,
+      CURLOPT_URL            => $configuration['tripay_host'] . '/transaction/create',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HEADER         => false,
+      CURLOPT_HTTPHEADER     => ['Authorization: ' . $auth],
+      CURLOPT_FAILONERROR    => false,
+      CURLOPT_POST           => true,
+      CURLOPT_POSTFIELDS     => http_build_query(json_decode($request->getBody()->getContents(), true)),
+      CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+    ]);
 
-  curl_close($curl);
+    $curlResult = curl_exec($curl);
+    $error = curl_error($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-  if (!empty($error)) return $error;
+    curl_close($curl);
 
-  return $response;
+    if (!empty($error)) {
+      $response->getBody()->write($error);
+      return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+    }
+
+    $resultData = json_decode($curlResult, true);
+    if ($resultData['success'] == false || $httpcode != 200 || $httpcode != 201) {
+      $response->getBody()->write($curlResult);
+      return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
+    }
+
+    $response->getBody()->write($curlResult);
+    return $response->withStatus(201)->withHeader('Content-Type', 'application/json');
+  } catch (Exception $e) {
+    $response->getBody()->write($e->getMessage());
+    return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+  }
 })->setName('transaction-create');
 
-$app->get('/merchant/payment-channel', function (Request $request, Response $response) {
+$app->get('/merchant/payment-channel', function (Request $request, Response $response) use ($configuration) {
 
-  $payload = ['code' => $request->getQueryParams()['code']];
+  $payload = [];
 
-  $curl = curl_init();
+  try {
+    if (array_key_exists('code', $request->getQueryParams())) {
+      $payload['code'] = $request->getQueryParams()['code'];
+    }
 
-  curl_setopt_array($curl, array(
-    CURLOPT_FRESH_CONNECT  => true,
-    CURLOPT_URL            => $this->host . '/merchant/payment-channel?' . http_build_query($payload),
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HEADER         => false,
-    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
-    CURLOPT_FAILONERROR    => false,
-    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
-  ));
+    $auth = isset($request->getHeader('Authorization')[0]) ? $request->getHeader('Authorization')[0] : '';
 
-  $response = curl_exec($curl);
-  $error = curl_error($curl);
+    $curl = curl_init();
 
-  curl_close($curl);
+    curl_setopt_array($curl, array(
+      CURLOPT_FRESH_CONNECT  => true,
+      CURLOPT_URL            => $configuration['tripay_host'] . '/merchant/payment-channel?' . http_build_query($payload),
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HEADER         => false,
+      CURLOPT_HTTPHEADER     => ['Authorization: ' . $auth],
+      CURLOPT_FAILONERROR    => false,
+      CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+    ));
 
-  if (!empty($error)) return $error;
+    $curlResult = curl_exec($curl);
+    $error = curl_error($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-  return $response;
+    curl_close($curl);
+
+    if (!empty($error)) {
+      $response->getBody()->write($error);
+      return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+    }
+
+    $resultData = json_decode($curlResult, true);
+    if ($resultData['success'] == false) {
+      $response->getBody()->write($curlResult);
+      return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
+    }
+
+    $response->getBody()->write($curlResult);
+    return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
+  } catch (Exception $e) {
+    $response->getBody()->write($e->getMessage());
+    return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+  }
 })->setName('merchant-payment-channel');
 
-$app->get('/payment/instruction', function (Request $request, Response $response) {
+$app->get('/payment/instruction', function (Request $request, Response $response) use ($configuration) {
 
-  $payload = ['code' => $request->getQueryParams()['code']];
+  $payload = [];
 
-  $curl = curl_init();
+  try {
+    if (array_key_exists('code', $request->getQueryParams())) {
+      $payload['code'] = $request->getQueryParams()['code'];
+    }
 
-  curl_setopt_array($curl, [
-    CURLOPT_FRESH_CONNECT  => true,
-    CURLOPT_URL            => $this->host . '/payment/instruction?' . http_build_query($payload),
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_HEADER         => false,
-    CURLOPT_HTTPHEADER     => ['Authorization: Bearer ' . $this->apiKey],
-    CURLOPT_FAILONERROR    => false,
-    CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
-  ]);
+    $auth = isset($request->getHeader('Authorization')[0]) ? $request->getHeader('Authorization')[0] : '';
 
-  $response = curl_exec($curl);
-  $error = curl_error($curl);
+    $curl = curl_init();
 
-  curl_close($curl);
+    curl_setopt_array($curl, [
+      CURLOPT_FRESH_CONNECT  => true,
+      CURLOPT_URL            => $configuration['tripay_host'] . '/payment/instruction?' . http_build_query($payload),
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_HEADER         => false,
+      CURLOPT_HTTPHEADER     => ['Authorization: ' . $auth],
+      CURLOPT_FAILONERROR    => false,
+      CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
+    ]);
 
-  if (!empty($error)) return $error;
+    $curlResult = curl_exec($curl);
+    $error = curl_error($curl);
+    $httpcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-  return $response;
+    curl_close($curl);
+
+    if (!empty($error)) {
+      $response->getBody()->write($error);
+      return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+    }
+
+    $resultData = json_decode($curlResult, true);
+    if ($resultData['success'] == false) {
+      $response->getBody()->write($curlResult);
+      return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
+    }
+
+    $response->withStatus(200)->getBody()->write($curlResult);
+    return $response->withHeader('Content-Type', 'application/json');
+  } catch (Exception $e) {
+    $response->getBody()->write($e->getMessage());
+    return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
+  }
 })->setName('payment-instruction');
 
 // Run app
