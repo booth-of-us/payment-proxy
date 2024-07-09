dee5d7b2a5f7f5771914d45e6f76e5a246308b46
aldiwildan77 <aldiwild77@gmail.com>
2024-07-09 21:50:30 +0700

    feat: add log file

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use Monolog\Logger;
+use Monolog\Handler\RotatingFileHandler;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Selective\BasePath\BasePathMiddleware;
@@ -10,6 +12,12 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $configuration = [
   'tripay_host' => 'https://tripay.co.id/api-sandbox'
 ];
+
+// Create the logger
+$logger = new Logger('app_logger');
+
+// Add RotatingFileHandler to log files and rotate every 7 days
+$logger->pushHandler(new RotatingFileHandler(__DIR__ . '/../logs/app.log', 7, Logger::DEBUG));
 
 $app = AppFactory::create();
 
@@ -23,16 +31,18 @@ $app->add(new BasePathMiddleware($app));
 $app->addErrorMiddleware(true, true, true);
 
 // Define app routes
-$app->get('/', function (Request $request, Response $response) {
+$app->get('/', function (Request $request, Response $response) use ($logger) {
+  $logger->info('Health Check route accessed');
   $response->getBody()->write('Health Check: OK');
   return $response;
 })->setName('root');
 
-$app->get('/merchant/fee-calculator', function (Request $request, Response $response) use ($configuration) {
+$app->get('/merchant/fee-calculator', function (Request $request, Response $response) use ($configuration, $logger) {
 
   $payload = [];
 
   try {
+    $logger->info('Merchant fee calculator route accessed', ['query' => $request->getQueryParams()]);
     if (array_key_exists('code', $request->getQueryParams())) {
       $payload['code'] = $request->getQueryParams()['code'];
     }
@@ -62,12 +72,14 @@ $app->get('/merchant/fee-calculator', function (Request $request, Response $resp
     curl_close($curl);
 
     if (!empty($error)) {
+      $logger->error('Curl error', ['error' => $error]);
       $response->withStatus(500)->getBody()->write($error);
       return $response->withHeader('Content-Type', 'application/json');
     }
 
     $resultData = json_decode($curlResult, true);
     if ($resultData['success'] == false) {
+      $logger->error('API call unsuccessful', ['response' => $resultData]);
       $response->getBody()->write($curlResult);
       return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
     }
@@ -75,14 +87,16 @@ $app->get('/merchant/fee-calculator', function (Request $request, Response $resp
     $response->getBody()->write($curlResult);
     return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
   } catch (Exception $e) {
+    $logger->error('Exception caught', ['exception' => $e->getMessage()]);
     $response->getBody()->write($e->getMessage());
     return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
   }
 })->setName('merchant-fee-calculator');
 
-$app->post('/transaction/create', function (Request $request, Response $response) use ($configuration) {
+$app->post('/transaction/create', function (Request $request, Response $response) use ($configuration, $logger) {
 
   try {
+    $logger->info('Transaction create route accessed', ['body' => $request->getBody()->getContents()]);
     $auth = isset($request->getHeader('Authorization')[0]) ? $request->getHeader('Authorization')[0] : '';
 
     $curl = curl_init();
@@ -106,12 +120,14 @@ $app->post('/transaction/create', function (Request $request, Response $response
     curl_close($curl);
 
     if (!empty($error)) {
+      $logger->error('Curl error', ['error' => $error]);
       $response->getBody()->write($error);
       return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
     }
 
     $resultData = json_decode($curlResult, true);
     if ($resultData['success'] == false || $httpcode != 200 || $httpcode != 201) {
+      $logger->error('API call unsuccessful', ['response' => $resultData]);
       $response->getBody()->write($curlResult);
       return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
     }
@@ -119,16 +135,18 @@ $app->post('/transaction/create', function (Request $request, Response $response
     $response->getBody()->write($curlResult);
     return $response->withStatus(201)->withHeader('Content-Type', 'application/json');
   } catch (Exception $e) {
+    $logger->error('Exception caught', ['exception' => $e->getMessage()]);
     $response->getBody()->write($e->getMessage());
     return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
   }
 })->setName('transaction-create');
 
-$app->get('/merchant/payment-channel', function (Request $request, Response $response) use ($configuration) {
+$app->get('/merchant/payment-channel', function (Request $request, Response $response) use ($configuration, $logger) {
 
   $payload = [];
 
   try {
+    $logger->info('Merchant payment channel route accessed', ['query' => $request->getQueryParams()]);
     if (array_key_exists('code', $request->getQueryParams())) {
       $payload['code'] = $request->getQueryParams()['code'];
     }
@@ -154,12 +172,14 @@ $app->get('/merchant/payment-channel', function (Request $request, Response $res
     curl_close($curl);
 
     if (!empty($error)) {
+      $logger->error('Curl error', ['error' => $error]);
       $response->getBody()->write($error);
       return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
     }
 
     $resultData = json_decode($curlResult, true);
     if ($resultData['success'] == false) {
+      $logger->error('API call unsuccessful', ['response' => $resultData]);
       $response->getBody()->write($curlResult);
       return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
     }
@@ -167,16 +187,18 @@ $app->get('/merchant/payment-channel', function (Request $request, Response $res
     $response->getBody()->write($curlResult);
     return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
   } catch (Exception $e) {
+    $logger->error('Exception caught', ['exception' => $e->getMessage()]);
     $response->getBody()->write($e->getMessage());
     return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
   }
 })->setName('merchant-payment-channel');
 
-$app->get('/payment/instruction', function (Request $request, Response $response) use ($configuration) {
+$app->get('/payment/instruction', function (Request $request, Response $response) use ($configuration, $logger) {
 
   $payload = [];
 
   try {
+    $logger->info('Payment instruction route accessed', ['query' => $request->getQueryParams()]);
     if (array_key_exists('code', $request->getQueryParams())) {
       $payload['code'] = $request->getQueryParams()['code'];
     }
@@ -185,7 +207,7 @@ $app->get('/payment/instruction', function (Request $request, Response $response
 
     $curl = curl_init();
 
-    curl_setopt_array($curl, [
+    curl_setopt_array($curl, array(
       CURLOPT_FRESH_CONNECT  => true,
       CURLOPT_URL            => $configuration['tripay_host'] . '/payment/instruction?' . http_build_query($payload),
       CURLOPT_RETURNTRANSFER => true,
@@ -193,7 +215,7 @@ $app->get('/payment/instruction', function (Request $request, Response $response
       CURLOPT_HTTPHEADER     => ['Authorization: ' . $auth],
       CURLOPT_FAILONERROR    => false,
       CURLOPT_IPRESOLVE      => CURL_IPRESOLVE_V4
-    ]);
+    ));
 
     $curlResult = curl_exec($curl);
     $error = curl_error($curl);
@@ -202,19 +224,22 @@ $app->get('/payment/instruction', function (Request $request, Response $response
     curl_close($curl);
 
     if (!empty($error)) {
+      $logger->error('Curl error', ['error' => $error]);
       $response->getBody()->write($error);
       return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
     }
 
     $resultData = json_decode($curlResult, true);
     if ($resultData['success'] == false) {
+      $logger->error('API call unsuccessful', ['response' => $resultData]);
       $response->getBody()->write($curlResult);
       return $response->withStatus($httpcode)->withHeader('Content-Type', 'application/json');
     }
 
-    $response->withStatus(200)->getBody()->write($curlResult);
-    return $response->withHeader('Content-Type', 'application/json');
+    $response->getBody()->write($curlResult);
+    return $response->withStatus(200)->withHeader('Content-Type', 'application/json');
   } catch (Exception $e) {
+    $logger->error('Exception caught', ['exception' => $e->getMessage()]);
     $response->getBody()->write($e->getMessage());
     return $response->withStatus(500)->withHeader('Content-Type', 'application/json');
   }
